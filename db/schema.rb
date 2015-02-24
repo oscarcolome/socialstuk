5e55c4dd4d7a65e3be0cfa3982bf6c3f43e7d2c8
Oscar <amrothceleb@gmail.com>
2015-02-24 11:00:43 +0100

    Added user extra attributes. made profile page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219162113) do
+ActiveRecord::Schema.define(version: 20150224095057) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "", null: false
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20150219162113) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "bio"
+    t.integer  "age"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
