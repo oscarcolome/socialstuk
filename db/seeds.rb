4d067b2f1312815fc7888d94699dcf7e584a4b39
Oscar <amrothceleb@gmail.com>
2015-02-24 12:41:08 +0100

    Created index action/route/view Added links to navbar Added seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'mariano', email: 'mariano@mariano.com', age: '38', gender: 'Male', bio: 'Just a musician/mathematic/philosopher/programmer/journalist', password: 'qwertyqwerty', password_confirmation: 'qwertyqwerty')
+User.create(username: 'boris', email: 'boris@boris.com', age: '90', gender: 'Male', bio: 'Just an older guy with a beard and stuff', password: 'qwertyqwerty', password_confirmation: 'qwertyqwerty')
+User.create(username: 'noplegate', email: 'noplegate@noplegate.com', age: '22', gender: 'Male', bio: 'A stupid teen', password: 'qwertyqwerty', password_confirmation: 'qwertyqwerty')
+User.create(username: 'tiago', email: 'tiago@tiago.com', age: '40', gender: 'Male', bio: 'The "Go Ahead" teacher', password: 'qwertyqwerty', password_confirmation: 'qwertyqwerty')
+User.create(username: 'vicente', email: 'vicente@vicente.com', age: '26', gender: 'Male', bio: 'Where people goes', password: 'qwertyqwerty', password_confirmation: 'qwertyqwerty')
