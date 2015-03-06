7315f16a0fe1b36a695ea5bc4234bee3ea42c002
Oscar <amrothceleb@gmail.com>
2015-03-06 12:57:46 +0100

    Friendships controller and helper.

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,29 @@
+class FriendshipsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:create]
+  before_action :set_friendship, only: [:destroy, :accept]
+
+  def create
+    @friendship = current_user.request_friendship(@user)
+    redirect_to users_path, notice: 'Friendship Created'
+  end
+
+  def destroy
+    @friendship.destroy
+    redirect_to users_path, notice: 'Friendship Deleted'
+  end
+
+  def accept
+    @friendship.accept_friendship
+    redirect_to users_path, notice: 'Friendship accepted'
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_friendship
+    @friendship = Friendship.find(params[:id])
+  end
+end
